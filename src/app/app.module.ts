2538b3290e05f3b99a134e69941650eeaccf79ab
Andrew Makar <andrii.makar@globaltechmakers.com>
2020-07-13 14:55:04 +0300

    Setup basic translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from 'src/app/app.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { MaterialModule } from 'src/app/material/material.module';
+import { TranslationModule } from './translation/translation.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +14,9 @@ import { MaterialModule } from 'src/app/material/material.module';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule,
+    TranslationModule,
   ],
   bootstrap: [AppComponent],
 })
