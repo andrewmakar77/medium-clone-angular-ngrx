ccba677e3cee84f454fb82d6e4f7b512c0010d65
Andrew Makar <andrii.makar@globaltechmakers.com>
2020-07-13 13:10:34 +0300

    Setup material theme

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from 'src/app/auth/components/register/register.component';
+import { AuthRoutingModule } from 'src/app/auth/auth-routing.module';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatInputModule],
 })
 export class AuthModule {}
