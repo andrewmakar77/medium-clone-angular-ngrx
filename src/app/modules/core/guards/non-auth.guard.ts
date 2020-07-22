00a77f8d65fb7b7dea87f13faac854159e0d7e00
Andrew Makar <andrew.makar13@gmail.com>
2020-07-22 22:34:16 +0300

    Add NonAuthGuard to prevent visiting login and register page, if user is authenticated

---
+++ src/app/modules/core/guards/non-auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { AuthFacade } from 'src/app/modules/auth/store/facades/auth.facade';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NonAuthGuard implements CanActivate {
+  constructor(private authFacade: AuthFacade) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return !this.authFacade.isLoaded$;
+  }
+}
